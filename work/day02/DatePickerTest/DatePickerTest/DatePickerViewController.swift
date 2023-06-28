314dbbf8334782402fa46a9e9de9ccb95e8399e2
Dongduk1 <dongduk1@Dongduk1ui-iMac.local>
2023-06-28 14:44:36 +0900

    add: day05 TabControllerTest

---
+++ work/day02/DatePickerTest/DatePickerTest/DatePickerViewController.swift
@@ -0,0 +1,81 @@
+//
+//  ViewController.swift
+//  DatePickerTest
+//
+//  Created by Dongduk1 on 2023/06/23.
+//
+
+import UIKit
+
+class DatePickerViewController: UIViewController {
+    let timeSelector: Selector = #selector(DatePickerViewController.updateTime)
+    let interval = 1.0
+    var count = 0
+    
+    let alarmSelector: Selector = #selector(DatePickerViewController.updateTime)
+    let alarmInterval = 60.0
+    var pick: String = ""
+    
+    var alaramTime: String = ""
+    
+    @IBOutlet var lblCurrentTime: UILabel!
+    @IBOutlet var lblPickerTime: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        Timer.scheduledTimer(timeInterval: interval, target: self, selector: timeSelector, userInfo: nil, repeats: true)
+//        Timer.scheduledTimer(timeInterval: alarmInterval, target: self, selector: alarmSelector, userInfo: nil, repeats: false)
+        
+    }
+
+    @IBAction func changeDatePicker(_ sender: UIDatePicker) {
+        let datePickerView = sender
+        
+        let formatter = DateFormatter()
+//        formatter.dateFormat = "yyyy-MM-dd HH:mm:SS EEE"
+        formatter.dateFormat = "hh:mm aaa"
+        alaramTime = formatter.string(from: datePickerView.date)
+        
+        lblPickerTime.text = "선택시간: " + formatter.string(from: datePickerView.date)
+        
+        pick = formatter.string(from: datePickerView.date)
+    }
+    
+    @objc func updateTime() {
+//        lblCurrentTime.text = String(count)
+//        count = count + 1
+        
+        let date = NSDate()
+        
+        let formatter = DateFormatter()
+//        formatter.dateFormat = "yyyy-MM-dd HH:mm:SS EEE"
+        formatter.dateFormat = "hh:mm aaa"
+        let currentTime = formatter.string(from: date as Date)
+        if (alaramTime == currentTime) {
+            let onAlert = UIAlertController(title: "알림", message: "설정한 시간입니다", preferredStyle: UIAlertController.Style.alert)
+            let onAction = UIAlertAction(title: "네, 알겠습니다", style: UIAlertAction.Style.default, handler: {
+                ACTION in self.alaramTime = ""
+            }) // alert 작동은 하는데, 네 누르면 1분 동안 알림 창 안 타나게 설정 해야함.
+            onAlert.addAction(onAction)
+            present(onAlert, animated: true, completion: nil)
+            
+            // 타이머를 하든 안 하든 2번 눌러야 alert창 꺼짐.
+            Timer.scheduledTimer(withTimeInterval: 60, repeats: false) { timer in
+                onAlert.dismiss(animated: true, completion: nil)
+                timer.invalidate()
+            }
+        }
+        
+        lblCurrentTime.text = "현재시간: " + formatter.string(from: date as Date)
+        
+//        let cur = formatter.string(from: NSDate() as Date)
+//        if (pick == cur) {
+//            view.backgroundColor = UIColor.red
+//        }
+        // 시간 맞으면 빨갛게 변하긴 하는데 안 돌아옴
+        
+    }
+}
+
---
+++ work/day05/TabControllerTest/TabControllerTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TabControllerTest
+//
+//  Created by Dongduk1 on 2023/06/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func btnMoveImageView(_ sender: UIButton) {
+        tabBarController?.selectedIndex = 1
+    }
+    
+    @IBAction func btnMoveDatePickerView(_ sender: UIButton) {
+        tabBarController?.selectedIndex = 2
+    }
+}
+
